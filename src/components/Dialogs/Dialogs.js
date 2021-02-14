2e45ea934279ec9bcc52266b70d3a4ac583bacc7
ruslan94t <p3triri@yandex.ru>
2021-02-14 18:48:49 +0300

    21-'Верстаем страницу Dialog after we need do NavLink Click to item chang active color and url'

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import s from './Dialogs.module.css'
 const Dialogs = () => {
-    return <div className={s.content}>
- dialog
-  </div>
+  return (
+
+
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Masha</div>
+        <div className={s.dialog}>Yana</div>
+        <div className={s.dialog}>Shaha</div>
+        <div className={s.dialog}>Alina</div>
+        <div className={s.dialog}>Juliya</div>
+        <div className={s.dialog}>Angelina</div>
+      </div>
+
+      <div className={s.messages}>
+        <div className={s.message}>how yiu</div>
+        <div className={s.message}>first man</div>
+        <div className={s.message}>protect KingDom</div>
+        <div className={s.message}>Most Know</div>
+        <div className={s.message}>Mortal Day</div>
+      </div>
+
+    </div>
+
+  )
 }
 
-export default Dialogs;
+export default Dialogs; 
